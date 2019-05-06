c17b08393e50244b4668e10b7b39376b0e247a8f
Michael Redig <mredig@gmail.com>
2019-05-06 11:37:35 -0500

    added main and students view controllers

---
+++ Students/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Students
+//
+//  Created by Michael Redig on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
